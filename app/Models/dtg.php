7b0592dc35dea459b76ecd8f55e8dd45005afd50
NuriKim0609 <kimnurihari@naver.com>
2023-08-14 16:57:50 +0900

    MapController 내의 dtg 테이블에서 가져온 position_x,y 가 0이 아닌 트립만 가져오는 함수 추가

--- app/Models/dtg.php
+++ app/Models/dtg.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class dtg extends Model
 {
-    use HasFactory;
+    protected $table = 'testdtg'; //dtg 테이블이 너무 무거워서 내가 임의로 가벼운 testdtg만들어서 로컬에서 테스트 한거얌!
 
-    // 데이터베이스 테이블 이름
-    protected $table = 'dtg'; 
-
-   // protected $primaryKey = ''; // 기본 키 컬럼 이름
+    public static function getCarPositions()
+    {
+        return self::select('car_id', 'position_x', 'position_y')
+            ->where('position_x', '<>', 0)
+            ->where('position_y', '<>', 0)
+            ->get();
+    }
 }
+?>
