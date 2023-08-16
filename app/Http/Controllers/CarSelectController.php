29de19610304c6cc0eb6f194c3585122d3951adc
NuriKim0609 <kimnurihari@naver.com>
2023-08-16 15:11:21 +0900

    CarSelectController: ajax & json complete

--- app/Http/Controllers/CarSelectController.php
+++ app/Http/Controllers/CarSelectController.php
@@ -6,26 +6,22 @@ use App\Models\car;
 use App\Models\schedule;
 use App\Models\driver;
 
-class CarSelectController extends MainController
+class CarSelectController
 {
-    public function car_select(Request $request) // $request는 ajax로 사용자의 입력값을 받아옴.
+    public function car_select(Request $request)
     {
+        $client = $request->input('client');
+        $vnum = $request->input('vnum');
+
         $d = date('N'); 
         $DayOfWeek = $d % 7 + 1; // 일요일이 1로 시작되는 요일
 
-        $query = car::where('VRN', 'like', '%' . $search . '%');
-        
-        if (is_numeric($request->input('request'))) {
-            $query->where('BIN', $request->input('request'));
-        } elseif ($request->input('request') === "default") {
-            // No additional filtering needed for 'BIN' column when 'default' is selected
-        }
-        
-        $result = $query->select('car_id', 'car_status', 'VRN')->get();
+        $commonQuery = car::where('BIN', $client)
+                            ->Where('VRN', 'like', '%' . $vnum . '%');
 
-        $car_id = $result->pluck('car_id');
-        $car_status = $result->pluck('car_status');
-        $VRN = $result->pluck('VRN');
+        $car_id = $commonQuery->pluck('car_id');
+        $car_status = $commonQuery->pluck('car_status');
+        $VRN = $commonQuery->pluck('VRN');
 
         $driver_code = schedule::select('driver_code')
             ->whereIn('car_id', $car_id)
@@ -36,6 +32,11 @@ class CarSelectController extends MainController
             ->whereIn('driver_code', $driver_code)
             ->pluck('driver_name');
         
-        return ['car_id' => $car_id, 'VRN' => $VRN, 'car_status' => $car_status, 'driver_name' => $driver_name];
+            return response()->json([
+                'car_status' => $car_status,
+                'VRN' => $VRN,
+                'driver_name' => $driver_name,
+            ]);
     }
-}?>
+}
+?>
