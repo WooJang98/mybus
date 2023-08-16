e59c88536386af7c809e25108bbe8e047ab5a0ae
NuriKim0609 <kimnurihari@naver.com>
2023-08-16 15:26:02 +0900

    MonitoringVideoController: ajax & json complete

--- app/Http/Controllers/MonitoringVideoController.php
+++ app/Http/Controllers/MonitoringVideoController.php
@@ -5,23 +5,21 @@ use Illuminate\Http\Request;
 
 use App\Models\monitoring_video;
 
-class MonitoringVideoController extends MainController
+class MonitoringVideoController
 {
-    public function show_today_video()//ajax 일 때는 괄호안에 Request $request 추가하기
+    public function show_today_video(Request $request)
     {
         $d = date('N'); 
         $DayOfWeek = $d % 7 + 1; // 일요일이 1로 시작되는 요일
 
-        //$car_id = $request->input('car_id'); // ajax request로 car_id 받는 변수
+        $car_id = $request->input('car_id'); 
 
-        //$videos = video::where('car_id', '=', $car_id) // 
-        $videos = monitoring_video::where('car_id', '=', 1) //ajax없이 local test 위한 줄
+        
+        $videos = monitoring_video::where('car_id', '=', $car_id) 
                    ->where('date', '=', $DayOfWeek) 
                    ->get();
         
-        return view('video', ['videos' => $videos]);
-        
-        //return response()->json($videos);
+        return response()->json($videos);
     }
 }
 ?>
