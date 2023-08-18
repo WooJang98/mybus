0b07285de0a1aafd15d0368475619c9f3055a77b
NuriKim0609 <kimnurihari@naver.com>
2023-08-18 18:33:21 +0900

    ReplayMapController update

--- app/Http/Controllers/ReplayMapController.php
+++ app/Http/Controllers/ReplayMapController.php
@@ -8,11 +8,8 @@ class ReplayMapController
 {
     public function getPositions(Request $request)
     {
-        $car_id = $request->input('car_id');
         $departure_time = $request->input('departure_time');
-        //$start_time = '2022-11-01 09:44:30.00'; // local test 용
 
-        //$carPositions = $this->getPositionData($start_time); // local test 용
         $carPositions = $this->getPositionData($departure_time);
 
         $formattedPositions = [];
@@ -23,7 +20,6 @@ class ReplayMapController
                 'position_y' => $position['position_y'] / 1000000,
             ];
         }
-        
         return response()->json($formattedPositions);
     } 
 
