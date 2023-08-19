1d854acb33f822ca33ab0e80db5a2c056524e955
NuriKim0609 <kimnurihari@naver.com>
2023-08-19 14:04:20 +0900

    MonitoringVideoController:complete, video.blade.php:update, public>videos folder:add

--- app/Http/Controllers/MonitoringVideoController.php
+++ app/Http/Controllers/MonitoringVideoController.php
@@ -13,13 +13,12 @@ class MonitoringVideoController
         $DayOfWeek = $d % 7 + 1; // 일요일이 1로 시작되는 요일
 
         $car_id = $request->input('car_id'); 
-
         
-        $monitoring_videos = monitoring_video::where('car_id',$car_id) 
-                   ->where('date', $DayOfWeek) 
+        $monitoring_videos = monitoring_video::where('car_id', $car_id) 
+                   ->where('date', $DayOfWeek)
                    ->get();
-        
-        return response()->json(['monitoring_videos' => $monitoring_videos]);
+
+        return response()->json($monitoring_videos);
     }
 }
 ?>
--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -12,53 +12,67 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
+<body>
+
 <div class="container-video-map">
-            
+    @foreach($monitoring_videos as $video)
+        @php
+            $outdoor_upper_path = asset($video->outdoor_upper_path);
+            $outdoor_lower_path = asset($video->outdoor_lower_path);
+            $indoor_upper_path = asset($video->indoor_upper_path);
+            $indoor_lower_path = asset($video->indoor_lower_path);
+        @endphp
+        
+        <div class="video-grid-item-monitor">
+            <div class="video-head-monitor">
+                <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
+                <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
+                <span class="video-head-text">"outdoor upper video"</span>
+            </div>
+            <video controls width="455px" height="268px">
+                <source src="{{ $outdoor_upper_path }}" type="video/mp4">
+            </video>
+        </div>
 
-                <div class="video-grid-item-monitor">
-                    <div class="video-head-monitor">
-                        <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
-                        <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
-                        <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
-                        <span class="video-head-text">"outdoor upper video"</span>
-                    </div>
-                    <video controls width="455px" height="268px">
-                        <source src="{{ asset($video->outdoor_upper_path) }}" type="video/mp4">
-                    </video>
-                </div>
+        <div class="video-grid-item-monitor">
+            <div class="video-head-monitor">
+                <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
+                <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
+                <span class="video-head-text">"outdoor lower video"</span>
+            </div>
+            <video controls width="455px" height="268px">
+                <source src="{{ $outdoor_lower_path }}" type="video/mp4">
+            </video>
+        </div>
 
-                <div class="video-grid-item-monitor">
-                    <div class="video-head-monitor">
-                        <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
-                        <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
-                        <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
-                        <span class="video-head-text">"outdoor lower video"</span>
-                    </div>
-                    <video controls width="455px" height="268px">
-                        <source src="{{ asset($video->outdoor_lower_path) }}" type="video/mp4">
-                    </video>
-                </div>
+        <div class="video-grid-item-monitor">
+            <div class="video-head-monitor">
+                <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
+                <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
+                <span class="video-head-text">"indoor upper video"</span>
+            </div>
+            <video controls width="455px" height="268px">
+                <source src="{{ $indoor_upper_path }}" type="video/mp4">
+            </video>
+        </div>
 
-                <div class="video-grid-item-monitor">
-                    <div class="video-head-monitor">
-                        <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
-                        <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
-                        <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
-                        <span class="video-head-text">"indoor upper video"</span>
-                    </div>
-                    <video controls width="455px" height="268px">
-                        <source src="{{ asset($video->indoor_upper_path) }}" type="video/mp4">
-                    </video>
-                </div>
+        <div class="video-grid-item-monitor">
+            <div class="video-head-monitor">
+                <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
+                <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
+                <span class="video-head-text">"indoor lower video"</span>
+            </div>
+            <video controls width="455px" height="268px">
+                <source src="{{ $indoor_lower_path }}" type="video/mp4">
+            </video>
+        </div>
+    @endforeach
+</div>
 
-                <div class="video-grid-item-monitor">
-                    <div class="video-head-monitor">
-                        <i class="fa-regular fa-circle-play" style="color: #ffffff;"></i>
-                        <i class="fa-regular fa-circle-stop" style="color: #ffffff;"></i>
-                        <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
-                        <span class="video-head-text">"indoor lower video"</span>
-                    </div>
-                    <video controls width="455px" height="268px">
-                        <source src="{{ asset($video->indoor_lower_path) }}" type="video/mp4">
-                    </video>
-                </div>
+</body>
+
+</html>
