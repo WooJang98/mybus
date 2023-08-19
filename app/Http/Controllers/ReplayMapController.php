cc422f2f5ad331f798068c9332e1867a914a4dfb
NuriKim0609 <kimnurihari@naver.com>
2023-08-19 10:33:18 +0900

    ReplayMapController >car_id add

--- app/Http/Controllers/ReplayMapController.php
+++ app/Http/Controllers/ReplayMapController.php
@@ -8,9 +8,10 @@ class ReplayMapController
 {
     public function getPositions(Request $request)
     {
+        $car_id = $request->input('car_id');
         $departure_time = $request->input('departure_time');
-
-        $carPositions = $this->getPositionData($departure_time);
+        
+        $carPositions = $this->getPositionData($car_id, $departure_time);
 
         $formattedPositions = [];
 
@@ -20,13 +21,41 @@ class ReplayMapController
                 'position_y' => $position['position_y'] / 1000000,
             ];
         }
+        
         return response()->json(['formattedPositions' => $formattedPositions]);
     } 
 
-
-    public function getPositionData($departure_time)
+    public function getPositionData($car_id, $departure_time)
     {
-        return (new MonitoringMapController)->getPositionData($departure_time);
+        $items_per_Page = 1000;
+        $page = 0;
+        $carPositions = [];
+        $startDataFound = false;
+
+        do {
+            $dtg_list = dtg::where('car_id', $car_id)
+                ->skip($page * $items_per_Page)
+                ->take($items_per_Page)
+                ->get();
+    
+            foreach ($dtg_list as $dtg) {
+                if ($dtg->departure_time == $departure_time) {
+                    $startDataFound = true; 
+                    if ($dtg->position_x != 0) {
+                        $carPositions[] = [
+                            'position_x' => $dtg->position_x,
+                            'position_y' => $dtg->position_y,
+                        ];
+                    }
+                } elseif ($startDataFound) {
+                    break 2;
+                }
+            } 
+            $page++;
+        } while (count($dtg_list) > 0);
+    
+        return $carPositions;
     }
 }
 ?>
+
